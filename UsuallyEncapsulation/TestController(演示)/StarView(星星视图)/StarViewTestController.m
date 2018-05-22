44cef6a523491fe35e69e5f6e23313c4678497cf
刘朋坤 <2562732740@qq.com>
2018-05-22 22:02:47 +0800

    添加演示图
    添加演示图

--- UsuallyEncapsulation/TestController(演示)/StarView(星星视图)/StarViewTestController.m
+++ UsuallyEncapsulation/TestController(演示)/StarView(星星视图)/StarViewTestController.m
@@ -21,16 +21,13 @@
     starView.starNumber = 2;
     [self.view addSubview:starView];
     
-    
     MStarView *starView1 = [[MStarView alloc] initWithFrame:CGRectMake(SCREEN_WIDTH/2-47.5, 250, 15, 15) starNumber:5 starSpace:5 starNormal:@"课程反馈_灰色星星" starSelect:@"课程反馈_星星"];
     starView1.starNumber = 4;
     [self.view addSubview:starView1];
     
-    
     MStarView *starView2 = [[MStarView alloc] initWithFrame:CGRectMake(SCREEN_WIDTH/2-95, 350, 15, 15) starNumber:10 starSpace:5 starNormal:@"课程反馈_灰色星星" starSelect:@"课程反馈_星星"];
     starView2.starNumber = 8;
     [self.view addSubview:starView2];
-    
     
     MStarView *starView3 = [[MStarView alloc] initWithFrame:CGRectMake(SCREEN_WIDTH/2-95, 450, 15, 15) starNumber:6 starSpace:20 starNormal:@"课程反馈_灰色星星" starSelect:@"课程反馈_星星"];
     starView3.starNumber = 4;
